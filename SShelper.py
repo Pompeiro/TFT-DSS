62d4b5c05e6a0eb9f71d6e08cb97a2e19ca802db
Pompeiro <pompeiro10@wp.pl>
2020-11-01 08:51:35 +0100

    Drawing rectangle on taken SS.

--- SShelper.py
+++ SShelper.py
@@ -29,7 +29,18 @@ wincap = WindowCapture('Spyder (Python 3.8)')
 ####################################
 ##################################
 
+line_color = (255, 0, 255)
+line_type = cv.LINE_4
+marker_color = (255, 0, 255)
+marker_type = cv.MARKER_CROSS
+marker_type_rectangle = cv.MARKER_SQUARE
 
+
+topLeft = (0, 0)
+bottomRight = (128, 128)
+center = (64, 64)
+
+marker_position = [topLeft, bottomRight, center]
 
 def make_ss_and_show(loadImage=0, window=wincap, croppingY=0, croppingX=0, croppingHeight=1080, croppingWidth=1920):
     if loadImage:
@@ -38,7 +49,13 @@ def make_ss_and_show(loadImage=0, window=wincap, croppingY=0, croppingX=0, cropp
         screenshot = window.get_screenshot()
     #print(screenshot)
     crop_img = screenshot[croppingY:croppingY+croppingHeight, croppingX:croppingX+croppingWidth]
-    cv.imshow("ss", crop_img)
+    # cv.imshow("ss", crop_img)
+    
+    cv.drawMarker(screenshot, marker_position[2], color=line_color,
+                  markerType=marker_type, markerSize=40, thickness=2)
+    cv.rectangle(screenshot, marker_position[0], marker_position[1], color=line_color,
+                         lineType=line_type, thickness=2)
+    cv.imshow("wind", screenshot)
     # OCRResult=reader.readtext(crop_img)
     # print(OCRResult)
     # listOfChampsToBuyThisTurn=sort_detected_champions_to_buy_by_position(OCRResult)
