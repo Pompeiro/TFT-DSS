12358bef95045ca69f94e4bcae4b9d9f3d631f5f
Pompeiro <pompeiro10@wp.pl>
2020-11-14 14:40:37 +0100

    New plan for modelValidator.

--- modelValidator.py
+++ modelValidator.py
@@ -6,6 +6,16 @@ Created on Mon Nov  2 20:04:21 2020
 
 This file should loop over every cropped screenshot to validate NN model
 
+
+Plan:
+    1. loading model.
+    2. ensure that model is ansering.
+    
+    3. load screenshot.
+    4. Crop screenshot and save it to directory.
+    5. Loop over every image with model.
+    6. print predictions on screenshot.
+
 """
 
 import os
@@ -13,12 +23,12 @@ import os
 from fastbook import *
 from fastai.vision.widgets import *
 
-# path = "C:\\Users\\janusz\\Documents\\TFT-DSS\\first"
-# path.ls(file_exts='.pth')
 
 
-bear_types = 'akali','aphelios','diana','lissandra','lux','sylas'
-path = Path('C:\\Users\\janusz\\Pictures\\tft\\images')
+
+bear_types = 'akali', 'annie', 'aphelios', 'ashe', 'cassiopeia', 'diana', 'evelynn', 'fiora', 'garen', 'hecarim', 'irelia', 'janna', 'jarvan', 'jax', 'jhin', 'jinx', 'kalista', 'kennen', 'kindred', 'lee', 'lissandra', 'lulu', 'lux', 'maokai', 'morgana', 'nami', 'nidalee', 'nunu', 'pyke', 'sejuani', 'sett', 'shen', 'sylas', 'tahm', 'talon', 'teemo', 'thresh', 'vayne', 'veigar', 'vi', 'warwick', 'wukong', 'xin', 'yasuo', 'yone', 'yummi', 'zed', 'zilean'
+
+path = Path('C:\\Users\\janusz\\Pictures\\tft\\testingimages\\testing')
 bears = DataBlock(
     blocks=(ImageBlock, CategoryBlock), 
     get_items=get_image_files, 
@@ -28,31 +38,35 @@ bears = DataBlock(
 dls = bears.dataloaders(path,batch_size=8)
 
 
-path = "C:\\Users\\janusz\\Documents\\TFT-DSS\\first"
+path = "C:\\Users\\janusz\\Documents\\TFT-DSS\\models\\modelChampsWithoutLVL"
 learn = cnn_learner(dls, resnet18, metrics=error_rate)
 
 learn_inf = learn.load(path) 
 
 mainDirectory = 'C:\\Users\\janusz\\Pictures\\tft\\testingimages\\graTest'
 
-u = os.listdir(mainDirectory)
-
-fullPathToSecondaryDirectoryList = []
-
-### fullPathToSecondaryDirectoryList filling
-
-for i,filename in enumerate(u):
-    print(filename)
-    fullPathToSecondaryDirectoryList.append(os.path.join(mainDirectory, filename))
-print(fullPathToSecondaryDirectoryList)
+print(learn_inf.predict('C:\\Users\\janusz\\Pictures\\tft\\testingimages\\graTest\\part10\\name00000026.jpg')[0])
 
 
-#### using model on every image
 
-for i,filename in enumerate(fullPathToSecondaryDirectoryList):
-    jpegsInSecondaryDirectory = os.listdir(fullPathToSecondaryDirectoryList[i])
-    for j,filename in enumerate(jpegsInSecondaryDirectory):
-        print(filename)
-        pathToJPG = os.path.join(fullPathToSecondaryDirectoryList[i], filename)
-        # print(pathToJPG)
-        print(learn_inf.predict(pathToJPG))
+# u = os.listdir(mainDirectory)
+
+# fullPathToSecondaryDirectoryList = []
+
+# ### fullPathToSecondaryDirectoryList filling
+
+# for i,filename in enumerate(u):
+#     print(filename)
+#     fullPathToSecondaryDirectoryList.append(os.path.join(mainDirectory, filename))
+# print(fullPathToSecondaryDirectoryList)
+
+
+# #### using model on every image
+
+# for i,filename in enumerate(fullPathToSecondaryDirectoryList):
+#     jpegsInSecondaryDirectory = os.listdir(fullPathToSecondaryDirectoryList[i])
+#     for j,filename in enumerate(jpegsInSecondaryDirectory):
+#         print(filename)
+#         pathToJPG = os.path.join(fullPathToSecondaryDirectoryList[i], filename)
+#         # print(pathToJPG)
+#         print(learn_inf.predict(pathToJPG))
