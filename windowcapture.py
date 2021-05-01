add295745455fb6d7c047ca8e9e6253e3ed608e4
Pompeiro <pompeiro10@wp.pl>
2021-05-01 07:02:14 +0200

    Added return in list_window_names()

--- windowcapture.py
+++ windowcapture.py
@@ -1,9 +1,17 @@
+from threading import Lock, Thread
+
 import numpy as np
-import win32gui, win32ui, win32con
+import win32con
+import win32gui
+import win32ui
 
 
 class WindowCapture:
 
+    # threading properties
+    stopped = True
+    lock = None
+    screenshot = None
     # properties
     w = 0
     h = 0
@@ -14,7 +22,10 @@ class WindowCapture:
     offset_y = 0
 
     # constructor
-    def __init__(self, window_name=None):
+    def __init__(self, window_name=None, cut_window=1):
+        # create a thread lock object
+        self.lock = Lock()
+
         # find the handle for the window we want to capture.
         # if no window name is given, capture the entire screen
         if window_name is None:
@@ -22,7 +33,7 @@ class WindowCapture:
         else:
             self.hwnd = win32gui.FindWindow(None, window_name)
             if not self.hwnd:
-                raise Exception('Window not found: {}'.format(window_name))
+                raise Exception("Window not found: {}".format(window_name))
 
         # get the window size
         window_rect = win32gui.GetWindowRect(self.hwnd)
@@ -30,8 +41,8 @@ class WindowCapture:
         self.h = window_rect[3] - window_rect[1]
 
         # account for the window border and titlebar and cut them off
-        border_pixels = 8
-        titlebar_pixels = 30
+        border_pixels = 8 * cut_window
+        titlebar_pixels = 30 * cut_window
         self.w = self.w - (border_pixels * 2)
         self.h = self.h - titlebar_pixels - border_pixels
         self.cropped_x = border_pixels
@@ -51,12 +62,18 @@ class WindowCapture:
         dataBitMap = win32ui.CreateBitmap()
         dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
         cDC.SelectObject(dataBitMap)
-        cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
+        cDC.BitBlt(
+            (0, 0),
+            (self.w, self.h),
+            dcObj,
+            (self.cropped_x, self.cropped_y),
+            win32con.SRCCOPY,
+        )
 
         # convert the raw data into a format opencv can read
-        #dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
+        # dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
         signedIntsArray = dataBitMap.GetBitmapBits(True)
-        img = np.fromstring(signedIntsArray, dtype='uint8')
+        img = np.fromstring(signedIntsArray, dtype="uint8")
         img.shape = (self.h, self.w, 4)
 
         # free resources
@@ -66,9 +83,9 @@ class WindowCapture:
         win32gui.DeleteObject(dataBitMap.GetHandle())
 
         # drop the alpha channel, or cv.matchTemplate() will throw an error like:
-        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type() 
+        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type()
         #   && _img.dims() <= 2 in function 'cv::matchTemplate'
-        img = img[...,:3]
+        img = img[..., :3]
 
         # make image C_CONTIGUOUS to avoid errors that look like:
         #   File ... in draw_rectangles
@@ -84,10 +101,15 @@ class WindowCapture:
     # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
     @staticmethod
     def list_window_names():
+        window_list = []
+
         def winEnumHandler(hwnd, ctx):
             if win32gui.IsWindowVisible(hwnd):
                 print(hex(hwnd), win32gui.GetWindowText(hwnd))
+                window_list.append(win32gui.GetWindowText(hwnd))
+
         win32gui.EnumWindows(winEnumHandler, None)
+        return window_list
 
     # translate a pixel position on a screenshot image to a pixel position on the screen.
     # pos = (x, y)
@@ -96,3 +118,23 @@ class WindowCapture:
     # the __init__ constructor.
     def get_screen_position(self, pos):
         return (pos[0] + self.offset_x, pos[1] + self.offset_y)
+
+    # threading methods
+
+    def start(self):
+        self.stopped = False
+        t = Thread(target=self.run)
+        t.start()
+
+    def stop(self):
+        self.stopped = True
+
+    def run(self):
+        # TODO: you can write your own time/iterations calculation to determine how fast this is
+        while not self.stopped:
+            # get an updated image of the game
+            screenshot = self.get_screenshot()
+            # lock the thread while updating the results
+            self.lock.acquire()
+            self.screenshot = screenshot
+            self.lock.release()
